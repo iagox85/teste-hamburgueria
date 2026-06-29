eddbb1861be66d842743f886d905e1326569e068
iagox85 <iagox85@gmail.com>
2026-06-29 14:27:15 -0300

    supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,4 +1,4 @@
-const supabase = window.supabase.createClient(
-  SUPABASE_CONFIG.url,
-  SUPABASE_CONFIG.publishableKey
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_CONFIG.url,
+    SUPABASE_CONFIG.publishableKey
 );
