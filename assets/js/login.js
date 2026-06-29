6d2d735d0da54e4b0b0276b45ac7cbb80214ad5e
iagox85 <iagox85@gmail.com>
2026-06-29 14:08:41 -0300

    login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,1 +1,24 @@
-// Login
+const formLogin = document.getElementById("formLogin");
+const mensagemLogin = document.getElementById("mensagemLogin");
+
+formLogin.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const senha = document.getElementById("senha").value.trim();
+
+  mensagemLogin.innerText = "Entrando...";
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: senha
+  });
+
+  if (error) {
+    mensagemLogin.innerText = "E-mail ou senha inválidos.";
+    return;
+  }
+
+  mensagemLogin.innerText = "Login realizado com sucesso!";
+  window.location.href = "admin.html";
+});
