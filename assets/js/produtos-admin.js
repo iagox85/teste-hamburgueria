68e620f00a6e10eeecca6e050d4f448d28a4b5ce
iagox85 <iagox85@gmail.com>
2026-06-29 16:45:34 -0300

    produtos-admin.js

--- assets/js/produtos-admin.js
+++ assets/js/produtos-admin.js
@@ -1,1 +1,14 @@
-// Produtos
+const formProduto = document.getElementById("formProduto");
+const listaProdutos = document.getElementById("listaProdutos");
+
+const LOJA_ID = null; // vamos ligar isso automaticamente depois
+
+formProduto.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const nome = document.getElementById("produtoNome").value.trim();
+  const descricao = document.getElementById("produtoDescricao").value.trim();
+  const preco = Number(document.getElementById("produtoPreco").value);
+
+  alert("Próximo passo: antes de salvar produto, precisamos criar a primeira loja no banco.");
+});
