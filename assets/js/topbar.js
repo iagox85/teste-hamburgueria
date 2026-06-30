4b2c4dbc6ee770e04b1f48f5d7a8e53b07f38509
iagox85 <iagox85@gmail.com>
2026-06-30 01:11:12 -0300

    topbar.js

--- assets/js/topbar.js
+++ assets/js/topbar.js
@@ -1,1 +1,15 @@
+function carregarTopbar(titulo, subtitulo) {
+  const topbar = document.getElementById("topbar");
 
+  topbar.innerHTML = `
+    <div>
+      <h1>${titulo}</h1>
+      <p>${subtitulo}</p>
+    </div>
+
+    <div class="topbar-actions">
+      <button class="notification">🔔</button>
+      <button id="sair">Sair</button>
+    </div>
+  `;
+}
